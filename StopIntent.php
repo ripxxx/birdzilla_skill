889fc742e7520d7cba5faa77aecc125222b8e7c0
Alexander Berdnikov <sasha@onix-systems.com>
2016-07-29 12:07:23 +0300

    stop intent fix

--- StopIntent.php
+++ StopIntent.php
@@ -3,11 +3,14 @@
 class StopIntent extends Intent {
     
     public function ask($params = array()) {
+        
         return $this->endSessionResponse();
     }
     
     public function run($params = array()) {
-        return $this->ask($params);
+        $response = new Response(flase);
+        $response->addText('What would you like to hear next?');
+        return $response;
     }
     
 }
